fa8f70882b8f01c66e758654ba9e93d21b9837be
vuyanitutwane <vuyaitutwane@outlook.com>
2020-02-20 15:42:34 +0200

    adding new files after my I've built and created my nunit test file

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -4,9 +4,13 @@ namespace Problems
 {
     class Program
     {
+        void Prefix(string input)
+        {
+            Console.WriteLine(input.Length + input.Split().Length + ",:" + input);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Prefix("vuyani");
         }
     }
 }
